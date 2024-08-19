e05184c98d444489d2837cb91d0b25364c39736d
me <me>
2024-08-19 11:00:28 +0100

    async_function

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" 1-concurrent_coroutines.py """
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    wait_n function
+    """
+    res= await asyncio.gather(
+        *(wait_random(max_delay) for i in range(n)))
+    return sorted(res)
