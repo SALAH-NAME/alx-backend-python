6c59fd2cded60356e35c34052dc5de1f4fb2c9fe
Salah <Salah>
2024-09-09 10:37:41 +0100

    unittests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -12,18 +12,17 @@ class TestGithubOrgClient(unittest.TestCase):
     """
 
     @parameterized.expand([
-        ("google", {"login": "google"}),
-        ("abc", {"login": "abc"})
+        ("google"),
+        ("abc")
     ])
     @patch('client.get_json')
-    def test_org(self, org_name, expected_response, mock_get_json):
+    def test_org(self, org_name, mock):
         """test_org function
         """
-        mock_get_json.return_value = expected_response
         client = GithubOrgClient(org_name)
-        self.assertEqual(client.org(), expected_response)
+        client.org()
         url = f"https://api.github.com/orgs/{org_name}"
-        mock_get_json.assert_called_once_with(url)
+        mock.assert_called_once_with(url)
 
     def test_public_repos_url(self):
         """test_public_repos_url function
