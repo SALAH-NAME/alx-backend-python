a7b0f09f4cba38330e63178c5ec5e8cbbd2fb5e6
Salah <Salah>
2024-09-09 11:10:48 +0100

    unittests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -36,6 +36,7 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(client._public_repos_url,
                              "https://api.github.com/orgs/google/repos")
 
+    @patch('client.get_json')
     def test_public_repos(self, mock_get_json):
         """test_public_repos function
         """
@@ -44,17 +45,13 @@ class TestGithubOrgClient(unittest.TestCase):
 
         with patch('client.GithubOrgClient._public_repos_url',
                    new_callable=PropertyMock) as mock_public_repos_url:
-            mock_public_repos_url.return_value = (
-                'https://api.github.com/orgs/google/repos'
-            )
+            mock_public_repos_url.return_value = 'worold'
             client = GithubOrgClient('google')
             result = client.public_repos()
-
-            self.assertEqual(result, ['repo1', 'repo2'])
-            mock_get_json.assert_called_once_with(
-                'https://api.github.com/orgs/google/repos'
-            )
+            names = [i["name"] for i in test_payload]
+            self.assertEqual(result, names)
             mock_public_repos_url.assert_called_once()
+            mock_get_json.assert_called_once()
 
     @parameterized.expand([
         ({"license": {"key": "my_license"}}, "my_license", True),
